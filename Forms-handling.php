ecc7a23da6a2494906be2604c1b636e90ef86f0b
Paul Slangen <plg.slangen@gmail.com>
2023-01-11 16:44:01 +0100

    PHP Forms (#2)

---
+++ Forms-handling.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+
+<!-- Post -->
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+<!-- Get -->
+<form action="welcome_get.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
